2f3e24f9f7e510c183267bd6e9eaad2b94d4dd09
joseph2792 <jalfaro@perceptiongrp.com>
2018-08-29 16:02:12 -0300

    archivos de bootstrap

--- index.php
+++ index.php
@@ -5,8 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>RedSocial - Futbol</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+    <!--navbar-->
+
+    <!--contenido de las secciones: login y registro home, amigos, faq, perfil-->
     
+    <!--footer-->
+    
+    <!-- Librería de jQuery -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <!-- Librería de JS de Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
