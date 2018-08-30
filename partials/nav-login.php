f8a33eed438ef0fa7e2dcb293ad6b366b11e39cf
joseph2792 <jalfaro@perceptiongrp.com>
2018-08-30 15:30:18 -0300

    estilos al nav-login

--- partials/nav-login.php
+++ partials/nav-login.php
@@ -1,12 +1,14 @@
-<nav class="cont-navLogin">
-    <div class="cont-logo">
-        <img src="../img/logo.svg" alt="logo">
-    </div>
-    <form action="">
-        <div class="cont-search">
-            <input type="search" placeholder="Buscar grupos o personas..." name="q" id="buscadorNav">
-            <i class="fa fa-user icon"></i>
+<header class="col-12 fixed-top">
+    <nav class="cont-navLogin">
+        <div class="cont-logo">
+            <img src="img/logo.svg" alt="logo">
         </div>
-    </form>
+        <form action="">
+            <div class="cont-search">
+                <input type="search" placeholder="Buscar grupos o personas..." name="q" id="buscadorNav">
+                <i class="fa fa-search icon"></i>
+            </div>
+        </form>
 
-</nav>
+    </nav>
+</header>
