041e33b9973d66f54eb23489a08170e1332c6c92
joseph2792 <jalfaro@perceptiongrp.com>
2018-08-31 14:27:46 -0300

    nav principal

--- partials/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-	<ul>
-		<li><a href="index.php">Home</a></li>		
-	</ul>
-	<h2>Copyright 2018</h2>
-</footer>
--- partials/header-nav.php
+++ partials/header-nav.php
@@ -1,15 +1,13 @@
 <header>
 	<nav>
+		<div class="cont-logo">
+            <img src="img/logo.svg" alt="logo">            
+        </div>
 		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="about.php">About</a></li>
-			<li><a href="services.php">Services</a></li>
-			<li><a href="portfolio.php">Portfolio</a></li>
-			<li><a href="blog.php">Blog</a></li>
-			<li><a href="contact.php">Contact</a></li>
-			<li><a href="#">Twitter</a></li>
-			<li><a href="#">Facebook</a></li>
-			<li><a href="#">Instagram</a></li>
+			<li><a href="index.php"><i class="fa fa-search icon"></i></a></li>
+			<li><a href=""><i class="fa fa-comments"></i></a></li>
+			<li><a href=""><i class="fa fa-users"></i></a></li>
+			<li><a href=""><i class="fa fa-home"></i></a></li>
 		</ul>
 	</nav>
 </header>
