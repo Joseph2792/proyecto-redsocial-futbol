3c2160e5a1848e5551eaddd6aa5822af5a699aad
joseph2792 <jalfaro@perceptiongrp.com>
2018-08-31 14:34:01 -0300

    merge de master bugs

--- partials/head.php
+++ partials/head.php
@@ -4,13 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
     <title> <?= isset($pageTitle) ? $pageTitle : 'PlayGroup'; ?> </title>  
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">  
-=======
-    <title> <?= isset($pageTitle) ? $pageTitle : 'PlayGroup'; ?> </title>    
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
->>>>>>> e4da4e3df837e2a49348c40e74df0d03e9ce573e
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
