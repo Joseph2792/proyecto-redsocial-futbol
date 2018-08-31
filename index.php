3c57c9aa0d33ca2e8ebf2d25041207444597ad2c
joseph2792 <jalfaro@perceptiongrp.com>
2018-08-31 13:47:12 -0300

    fixed de la rama

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/bootstrap-grid.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <title>PlayGroups - Inicio</title>
-  </head>
-  <body>
-    <header>
-      <?php
-        require_once "partials/header-nav.php";
-      ?>
-    </header>
-    <br>
-    <div class="col-6">
+<?php
+	$pageTitle = "Home";
+	include "partials/head.php";
+?>
+    <!--navbar login-->
+    <?php require_once "partials/nav-login.php"; ?>
+    <!--navbar-->
+    <?php require_once "partials/header-nav.php"; ?>
+
+    <!--contenido de las secciones: login y registro home, amigos, faq, perfil-->
+    <div class="col-ms-12 col-md-12 col-lg-6">
       <div class="tipodepost-container">
         <ul>
           <li><a href="#" class="iconos"><i class="far fa-futbol"></i>Nuevo Partido</a></li>
@@ -26,7 +19,7 @@
     </div>
     <br><br>
 
-      <div class="col-6">
+      <div class="col-ms-12 col-md-12 col-lg-6">
         <div class="post-container">
           <div class="post-description">
             <img src="img/icons/trofeo.png" class="event-icon-trophy">
@@ -59,7 +52,7 @@
 
       </div>
       <br>
-      <div class="col-6">
+      <div class="col-ms-12 col-md-12 col-lg-6">
         <div class="post-container">
           <div class="post-description">
             <img src="img/icons/pelota.png" class="event-icon-ball">
@@ -89,24 +82,6 @@
             <i class="fas fa-comment-dots col-1"></i>
           </footer>
         </div>
-
-    <<footer><!-- ACA VA EL FOOTER HECHO EN PHP -->
-    </footer>
-  </body>
-</html>
-
-
-<?php
-	$pageTitle = "Home";
-	include "partials/head.php";
-?>
-    <!--navbar login-->
-    <?php require_once "partials/nav-login.php"; ?>
-    <!--navbar-->
-    <?php require_once "partials/header-nav.php"; ?>
-
-    <!--contenido de las secciones: login y registro home, amigos, faq, perfil-->
-
     <!--fin del contenido-->
 
     <!--scripts ed jquery y bootstrap-->
