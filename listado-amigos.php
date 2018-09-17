01abd263b63e2f117b352784a9dc5f65c8d64f3a
Jose Alfaro <jalfaro@perceptiongrp.com>
2018-09-17 08:25:26 -0300

    listado de amigos

--- listado-amigos.php
+++ listado-amigos.php
@@ -1,9 +1,9 @@
 <?php
-	$pageTitle = "Home";
+	$pageTitle = "Amigos";
 	include "partials/head.php";
 ?>
     <!--navbar-->
-    <?php require_once "partials/header-nav.php"; ?>
+    <?php require_once "partials/nav-login.php"; ?>
     <div class="contenedor container">
     <!--contenido de las secciones: login y registro home, amigos, faq, perfil-->
         <section class="row cont-header-friends">
@@ -21,8 +21,32 @@
         </section>
         <section class="row cont-friends">
             <ul>
-                <li>
-
+                <li class="cont-friend">
+                    <div class="card-friend">
+                        <a class="cont-photo-friend" href="">
+                            <img class="photo-friend" src="img/man.jpg" alt="">
+                            <span>Andres Navarro</span>
+                        </a>
+                        <div class="cont-info-friend">
+                            <div class="dato-friend">
+                                <span><strong>Partidos jugados</strong></span>
+                                <span>120</span>
+                            </div>
+                            <div class="dato-friend">
+                                <span><strong>Torneos jugados</strong></span>
+                                <span>20</span> 
+                            </div>           
+                            <div class="dato-friend">
+                                <span><strong>Habilidad</strong></span>
+                                <span>estrellas</span>
+                            </div>
+                            <div class="msj-friend">
+                                <a class="ico-nav" href="" title="messenger">
+                                    <i class="fa fa-question-circle"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </li>
             </ul>
         </section>
