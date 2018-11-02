8ec4e284e853e6b42f8c357a70436da67b2b427a
joseph2792 <jalfaro@perceptiongrp.com>
2018-11-02 15:13:24 -0300

    migrate

---
+++ admin.php
@@ -0,0 +1,23 @@
+<?php
+    require_once '/migrate/migrate.php';
+
+	$pageTitle = "Admin";
+	include "partials/head.php";
+?>
+<!--navbar-->
+<?php require_once "partials/nav-login.php"; ?>
+<div class="contenedor container">
+<!--contenido de las secciones: login y registro home, amigos, faq, perfil-->
+    <section>       
+    </section>
+<!--fin del contenido-->
+    <div class="msn">
+        <a href="#" title="Chat (0)" data-toggle="popover" data-placement="left" data-content="No hay mensajes por leer">
+            <i class="fas fa-envelope"></i>
+            <span class="numb"></span>
+        </a>
+    </div>
+
+</div>
+<!--scripts ed jquery y bootstrap-->
+<?php require_once "partials/scripts.php"; ?>
